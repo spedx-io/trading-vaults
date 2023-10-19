fa2debd58d25997715baa0d3b7a180fd5ff40e47
akshatcoder-hash <akshatcoder@outlook.com>
2023-10-19 21:35:56 +0530

    added deposit and withdraw logic

--- programs/trading-vaults/src/lib.rs
+++ programs/trading-vaults/src/lib.rs
@@ -6,12 +6,28 @@ declare_id!("Eqs2vuTeCMLFhULBgh5f2TDuRCrYFecfgJxzUSGLRt21");
 #[program]
 pub mod trading_vaults {
     use super::*;
-    
+
     pub fn initialize(ctx: Context<Initialize>, initial_balance: u64) -> ProgramResult {
         let vault = &mut ctx.accounts.vault;
         vault.owner = *ctx.accounts.owner.key;
         vault.balance = initial_balance;
         Ok(())
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> ProgramResult {
+        let vault = &mut ctx.accounts.vault;
+        vault.balance += amount;
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> ProgramResult {
+        let vault = &mut ctx.accounts.vault;
+        if vault.balance >= amount {
+            vault.balance -= amount;
+            Ok(())
+        } else {
+            Err(ProgramError::Custom(1)) // Insufficient balance
+        }
     }
 }
 
@@ -22,6 +38,20 @@ pub struct Initialize<'info> {
     #[account(mut)]
     pub owner: Signer<'info>,
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+    #[account(mut, has_one = owner)]
+    pub vault: Account<'info, Vault>,
+    pub owner: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut, has_one = owner)]
+    pub vault: Account<'info, Vault>,
+    pub owner: Signer<'info>,
 }
 
 #[account]
