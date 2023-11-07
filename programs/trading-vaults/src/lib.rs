ae1292366cae4f5a81bb28d65a48e78dbd2dfb6b
akshatcoder-hash <akshatcoder@outlook.com>
2023-11-07 14:55:25 +0530

    trg initiate

--- programs/trading-vaults/src/lib.rs
+++ programs/trading-vaults/src/lib.rs
@@ -5,6 +5,7 @@ declare_id!("Eqs2vuTeCMLFhULBgh5f2TDuRCrYFecfgJxzUSGLRt21");
 
 #[program]
 pub mod trading_vaults {
+    use std::str::FromStr;
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>, initial_balance: u64) -> ProgramResult {
@@ -34,6 +35,89 @@ pub mod trading_vaults {
             Err(ProgramError::Custom(1)) // Insufficient balance
         }
     }
+    pub fn initialize_trader_risk_group(
+        ctx: Context<InitializeTraderRiskGroup>,
+        // You can include additional parameters if needed
+    ) -> ProgramResult {
+        let trg_account = &mut ctx.accounts.trader_risk_group;
+    
+        // Initialize the account with default or provided values
+        trg_account.owner = *ctx.accounts.user.key; // Owner's pubkey
+        trg_account.market_product_group = Pubkey::from_str("FUfpR31LmcP1VSbz5zDaM7nxnH55iBHkpwusgrnhaFjL").unwrap();
+        trg_account.positions = Vec::new(); // Start with an empty vector for positions
+        trg_account.open_orders = Vec::new(); // Start with an empty vector for orders
+        trg_account.cash_deposits = 0; // Start with zero cash deposits
+        
+        Ok(())
+    }    
+}
+
+#[account]
+pub struct TraderRiskGroup {
+    pub owner: Pubkey,
+    pub market_product_group: Pubkey,
+    pub positions: Vec<Position>, // Use a vector to store multiple positions.
+    pub open_orders: Vec<OpenOrder>, // Use a vector for open orders.
+    pub cash_deposits: u64,
+    //TODO: Additional fields as necessary.
+}
+
+impl TraderRiskGroup {
+    // Assuming a constant size for the number of positions and orders for simplification
+    // Change the '100' and '50' to reflect the real anticipated maximums of your design
+    pub const MAX_POSITIONS: usize = 100; 
+    pub const MAX_OPEN_ORDERS: usize = 50;
+
+    // Define the size of a Position, including any padding required by your specific data alignment needs
+    // This is a simplified example calculation. You might need to consider additional factors
+    // such as potential alignment padding between elements.
+    pub const POSITION_SIZE: usize = 32 + 8 + 8; // Size of asset Pubkey + quantity + entry_price
+
+    // Define the size of an OpenOrder similarly to Position
+    pub const OPEN_ORDER_SIZE: usize = 1 + 8 + 8 + 1; // Size of order_type + size + price + state
+
+    // This LEN constant would then encapsulate the space computation based on these sizes
+    pub const LEN: usize = 8 + // Discriminator
+                            32 + // Owner pubkey size
+                            32 + // Market product group pubkey size
+                            8 + Self::MAX_POSITIONS * Self::POSITION_SIZE + // Size and space for positions
+                            8 + Self::MAX_OPEN_ORDERS * Self::OPEN_ORDER_SIZE + // Size and space for orders
+                            8; // Cash deposits field size
+}
+
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
+pub struct Position {
+    pub asset: Pubkey,      // The asset this position is in.
+    pub quantity: u64,      // The quantity of the asset.
+    pub entry_price: u64,   // The price at which the position was entered.
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
+pub struct OpenOrder {
+    // Define the structure of an open order according to the requirements.
+    pub order_type: OrderType, // Enum for order type (e.g., limit, market).
+    pub size: u64,             // Size of the order.
+    pub price: u64,            // Order price.
+    pub state: OrderState,     // Enum for the state of the order.
+    //TODO: Include additional order details as required.
+}
+
+// Enums to represent order types and states might be defined as:
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
+pub enum OrderType {
+    Limit,
+    Market,
+    // Possibly others, like stop-loss or take-profit.
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
+pub enum OrderState {
+    Open,
+    PartiallyFilled,
+    Filled,
+    Cancelled,
+    //TODO: Any other states that might be relevant.
 }
 
 #[derive(Accounts)]
@@ -43,6 +127,16 @@ pub struct Initialize<'info> {
     #[account(mut)]
     pub owner: Signer<'info>,
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeTraderRiskGroup<'info> {
+    #[account(init, payer = user, space = TraderRiskGroup::LEN)]
+    pub trader_risk_group: Account<'info, TraderRiskGroup>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    //TODO: Add other accounts needed for initialization here
 }
 
 #[derive(Accounts)]
@@ -64,4 +158,5 @@ pub struct Vault {
     pub owner: Pubkey,
     pub balance: u64,
     pub is_depositor: bool,
+    pub trader_risk_group: Pubkey, 
 }
